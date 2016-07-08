8876c9f3ef7ca8a4522dd678f92d8fa1703636ac
Charles R. Portwood II <charlesportwoodii@erianna.com>
2016-07-08 18:17:33 -0500

    Adding argon2_get_info function

--- php_argon2.h
+++ php_argon2.h
@@ -4,7 +4,11 @@
 #define argon2_module_ptr &argon2_module_entry
 #define phpext_argon2_ptr argon2_module_ptr
 
-#define PHP_ARGON2_VERSION "1.0.2"
+#define PHP_ARGON2_VERSION "1.0.4"
+
+#define ARGON2_MEMORY_COST 1<<16
+#define ARGON2_TIME_COST 3
+#define ARGON2_THREADS 1
 
 #ifdef ZTS
 #include "TSRM.h"
